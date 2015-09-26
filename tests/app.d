6a0b5fab19ba09462306682255dc6aa1e6c735eb
David J. Kordsmeier <dkords@gmail.com>
2015-09-25 20:39:49 -0700

    Move the tests

--- tests/app.d
+++
@@ -1,9 +0,0 @@
-import unit_threaded.runner;
-
-import std.stdio;
-import tests.integration;
-
-int main(string[] args) {
-    writeln("Unit-threaded examples: Passing\n");
-    return runTests!(tests.integration)(args);
-}
